75b9bbf6fa89e559ceba78991a0eb060c61314ca
Opoola Yusuf <56214045+Yuskhosmith@users.noreply.github.com>
2020-03-27 11:14:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//single line comment
+let name = "Yusuf";
+const data = [Frontend, Design];
+console.log("My name is: " + name);
+console.log("My Courses are: " + data);
+for (let i=0; i<=200; i++){
+    if (i%2===0){
+        data.push(i);
+    }
+}
+console.log(data);
